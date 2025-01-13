8d87586a648dc7a34b8427a7f94dc63e69dbd3d3
NanduNanduz <nandunsb99@gmail.com>
2025-01-14 00:39:44 +0530

    Initial commit

---
+++ BlogAppFrontend/src/axiosInterceptor.js
@@ -0,0 +1,22 @@
+import axios  from "axios";
+
+// for each req injecting token for that created axiosInstance
+const axiosInstance = axios.create({
+    baseURL:'http://localhost:3000'
+})
+
+axiosInstance.interceptors.request.use((config)=>{
+    const accessToken = sessionStorage.getItem('logintoken');
+    if(accessToken){
+        if(config){
+            config.headers.token = accessToken;
+        }
+    }
+    return config
+},(error)=>{
+    return Promise.reject(error);
+})
+
+
+export default axiosInstance
+//all axios req is replace with axiosInstance
