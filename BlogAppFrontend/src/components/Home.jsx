57425e251a641d72b0ca8a813ca8579cfdd65f01
NanduNanduz <nandunsb99@gmail.com>
2025-01-14 01:07:27 +0530

    Delete blog

--- BlogAppFrontend/src/components/Home.jsx
+++ BlogAppFrontend/src/components/Home.jsx
@@ -13,11 +13,11 @@ import { useNavigate } from "react-router-dom";
 import axiosInstance from "../axiosInterceptor";
 
 const Home = () => {
-  const [cardData,setData] = useState([]);
+  const [cardData, setData] = useState([]);
 
   const navigate = useNavigate();
 
-  useEffect(()=>{
+  useEffect(() => {
     // '/blogs' - in the get of blog routes
     axiosInstance
       .get("http://localhost:3000/blog/blogs")
@@ -27,16 +27,31 @@ const Home = () => {
       .catch((error) => {
         console.log(error);
       });
-  },[])
+  }, []);
 
-//instead of row using parameter val
-  function update_data(val){
+  //instead of row using parameter val
+  function update_data(val) {
     // passing data to the add blogs page using val parameter
-    // state key word work as a prop to pass data with navigate 
-    navigate("/addblogs", {state: {val}});
-
+    // state key word work as a prop to pass data with navigate
+    navigate("/addblogs", { state: { val } });
   }
-  
+
+  // Delete a blog
+  const deleteBlog = (id) => {
+    if (window.confirm("Are you sure you want to delete this blog?")) {
+      axiosInstance
+        .delete(`/blog/deleteblog/${id}`)
+        .then((res) => {
+          alert("Blog deleted successfully!");
+          // Remove the deleted blog from the state
+          setData((prevData) => prevData.filter((blog) => blog._id !== id));
+        })
+        .catch((error) => {
+          console.error("Error deleting blog:", error);
+          alert("Failed to delete the blog.");
+        });
+    }
+  };
 
   return (
     <div style={{ margin: "5%" }}>
@@ -70,7 +85,12 @@ const Home = () => {
                   {/* update_data is the fun name and row is the data of each card  */}
                   Update
                 </Button>
-                <Button size="small" color="error" variant="contained" >
+                <Button
+                  size="small"
+                  color="error"
+                  variant="contained"
+                  onClick={() => deleteBlog(row._id)}
+                >
                   Delete
                 </Button>
               </CardActions>
