044515aa39b23b76c7494674264063b974e31302
NanduNanduz <nandunsb99@gmail.com>
2025-01-14 14:27:14 +0530

    created private routes

--- BlogAppFrontend/src/components/PrivateRoutes.jsx
+++ BlogAppFrontend/src/components/PrivateRoutes.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom';
 
 const PrivateRoutes = () => {
+
+    const token = sessionStorage.getItem('logintoken');
+    let verifyUser = false;
+    if(token){
+        verifyUser=true;
+    }
   return (
     <div>
-      
+        {/* outlet is a respective component .it will load when the user is verified */}
+      verifyUser?<Outlet/>:<Navigate to = {'/'}/>
     </div>
   )
 }
